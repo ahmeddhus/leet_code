56d647109bb35bdaaa4981e8481ff58b132d69d6
Ahmed Hussein <ahmeddhus@gmail.com>
2022-09-22 15:22:39 +0200

    Time: 61 ms (5.11%), Space: 53.4 MB (5.19%) - LeetHub

--- 9-palindrome-number/9-palindrome-number.java
+++ 9-palindrome-number/9-palindrome-number.java
@@ -1,9 +1,9 @@
 class Solution {
     public boolean isPalindrome(int x) {
-        if(x < 0 && x%10 == 0){
-          return false;
-        }else if(x < 10 && x >= 0){
+         if(x < 10 && x >= 0){
           return true;
+        }else if(x < 0 && x%10 == 0){
+          return false;
         }else{
           String[] numbersList = (x+"").split("");
           int lastIndex = numbersList.length - 1;
