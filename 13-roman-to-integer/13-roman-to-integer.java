daef04a3c07371bd7ab5257d1dd8d12c144e0961
Ahmed Hussein <ahmeddhus@gmail.com>
2022-09-23 13:58:12 +0200

    Time: 7 ms (73.26%), Space: 44.8 MB (81.48%) - LeetHub

--- 13-roman-to-integer/13-roman-to-integer.java
+++ 13-roman-to-integer/13-roman-to-integer.java
@@ -1,19 +1,19 @@
 class Solution {
     public int romanToInt(String s) {
-      if(s.length() == 1) return getRomantIntMap().get(s);
+      if(s.length() == 1) return getIntValueFromRoman(s.toCharArray()[0]);
 
       int number = 0;
 
       for(int i =0 ; i <s.length(); i+=2){
         if(i >= s.length()-1) {
           if(i== s.length()-1){
-            number += getRomantIntMap().get(s.charAt(i)+"");
+            number += getIntValueFromRoman(s.charAt(i));
           } 
           break;
         }
         
-        int firstNum = getRomantIntMap().get(s.charAt(i)+"");
-        int secondNum = getRomantIntMap().get(s.charAt(i+1)+"");
+        int firstNum = getIntValueFromRoman(s.charAt(i));
+        int secondNum = getIntValueFromRoman(s.charAt(i+1));
 
         if(firstNum >= secondNum){
           number += firstNum;
@@ -25,17 +25,14 @@ class Solution {
       return number;
     }
     
-    public  HashMap<String, Integer> getRomantIntMap(){
-      HashMap<String, Integer> romanIntMap = new HashMap<String, Integer>();
 
-      romanIntMap.put("I", 1);
-      romanIntMap.put("V", 5);
-      romanIntMap.put("X", 10);
-      romanIntMap.put("L", 50);
-      romanIntMap.put("C", 100);
-      romanIntMap.put("D", 500);
-      romanIntMap.put("M", 1000);
-
-      return romanIntMap;
-    }
+    public int  getIntValueFromRoman(char c){
+      if(c=='I') return 1;
+      else if(c=='V') return 5;
+      else if(c=='X') return 10;
+      else if(c=='L') return 50;
+      else if(c=='C') return 100;
+      else if(c=='D') return 500;
+      else return 1000;
+  }
 }
