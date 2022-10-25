904536d7b5bf35d6b3af690798df234065765aed
Ahmed Hussein <ahmeddhus@gmail.com>
2022-10-25 10:10:05 +0200

    Time: 3 ms (21.99%), Space: 42.6 MB (14.47%) - LeetHub

--- 1662-check-if-two-string-arrays-are-equivalent/1662-check-if-two-string-arrays-are-equivalent.java
+++ 1662-check-if-two-string-arrays-are-equivalent/1662-check-if-two-string-arrays-are-equivalent.java
@@ -1,5 +1,16 @@
 class Solution {
     public boolean arrayStringsAreEqual(String[] word1, String[] word2) {
-        return(String.join("", word1).equals(String.join("", word2)));
+        String word1Str = "";
+        String word2Str = "";
+
+        for (int i = 0; i < word1.length || i < word2.length; i++) {
+            if (i < word1.length)
+                word1Str += word1[i];
+            if (i < word2.length)
+                word2Str += word2[i];
+        }
+
+
+        return word1Str.equals(word2Str);
     }
 }
