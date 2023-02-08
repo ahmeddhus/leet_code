5709f3463afbc699ac29bed6733d910f1f581e7f
Ahmed Hussein <ahmeddhus@gmail.com>
2023-02-08 13:33:29 +0200

    Time: 44 ms (5.16%), Space: 42.5 MB (7.93%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -1,19 +1,12 @@
 class Solution {
     public boolean isValid(String s) {
-        final Stack<Character> stack = new Stack<>();
-        
-        for (char c : s.toCharArray()) {
-            if (c == ')' && !stack.isEmpty() && stack.peek() == '(') {
-                stack.pop();
-            } else if (c == '}' && !stack.isEmpty() && stack.peek() == '{') {
-                stack.pop();
-            } else if (c == ']' && !stack.isEmpty() && stack.peek() == '[') {
-                stack.pop();
-            } else {
-                stack.push(c);
-            }
-        }
-
-        return stack.isEmpty();
+        int length;
+    
+        do {
+            length = s.length();
+            s = s.replace("()", "").replace("{}", "").replace("[]", "");
+        } while(length != s.length());
+    
+        return s.length() == 0;
     }
 }
