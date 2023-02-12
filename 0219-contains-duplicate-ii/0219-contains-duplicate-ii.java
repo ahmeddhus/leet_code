bfa12437abccd891fca7543e194c640d7e434e98
Ahmed Hussein <ahmeddhus@gmail.com>
2023-02-12 15:50:42 +0200

    Time: 15 ms (97.94%), Space: 51.5 MB (50.50%) - LeetHub

--- 0219-contains-duplicate-ii/0219-contains-duplicate-ii.java
+++ 0219-contains-duplicate-ii/0219-contains-duplicate-ii.java
@@ -1,5 +1,8 @@
 class Solution {
     public boolean containsNearbyDuplicate(int[] nums, int k) {
+        if (nums.length == 0)
+            return false;
+
         Set<Integer> set = new HashSet<>();
         for (int i = 0; i < nums.length; i++) {
             if (i > k) {
