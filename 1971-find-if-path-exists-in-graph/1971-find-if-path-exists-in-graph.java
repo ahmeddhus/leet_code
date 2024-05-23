b1deeb172468e0b9703e9ed6561a251ac26a92ad
Ahmed Hussein <ahmeddhus@gmail.com>
2024-05-23 13:26:15 +0300

    Time: 350 ms (5.07%), Space: 190.5 MB (20.22%) - LeetHub

--- 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.java
+++ 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.java
@@ -1,30 +1,32 @@
 class Solution {
     public boolean validPath(int n, int[][] edges, int source, int destination) {
-        if (source == destination) return true; // Quick check if source and destination are the same
 
-        // Create adjacency list
-        List<List<Integer>> adjList = new ArrayList<>(n);
+        if (source == destination) return true;
+        
+        HashMap<Integer, List<Integer>> nodeMap = new HashMap<>();
+
         for (int i = 0; i < n; i++) {
-            adjList.add(new ArrayList<>());
+            nodeMap.put(i, new ArrayList<>());
         }
 
         // Populate adjacency list
         for (int[] edge : edges) {
-            adjList.get(edge[0]).add(edge[1]);
-            adjList.get(edge[1]).add(edge[0]); // Since the graph is undirected
+            nodeMap.get(edge[0]).add(edge[1]);
+            nodeMap.get(edge[1]).add(edge[0]);
         }
 
-        // BFS initialization
+        System.out.println("nodeMap => " + nodeMap);
+
         boolean[] visited = new boolean[n];
-        Queue<Integer> queue = new LinkedList<>();
+        final Queue<Integer> queue = new LinkedList<>();
 
         visited[source] = true;
         queue.add(source);
 
         while (!queue.isEmpty()) {
-            int vertex = queue.poll();
+            final int vertex = queue.poll();
 
-            for (int adjVertex : adjList.get(vertex)) {
+            for (int adjVertex : nodeMap.get(vertex)) {
                 if (adjVertex == destination) {
                     return true;
                 }
@@ -34,6 +36,7 @@ class Solution {
                 }
             }
         }
-        return false; 
+
+        return false;
     }
 }
