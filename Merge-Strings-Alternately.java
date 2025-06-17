130fef0782c4f2b24839dc4421986c3eab65138b
Ahmed Hussein <ahmeddhus@gmail.com>
2025-06-17 07:36:23 +0300

    [1768] [Time Beats: 81.54%] [Memory Beats: 72.19%] - LeetPush

--- Merge-Strings-Alternately.java
+++ Merge-Strings-Alternately.java
@@ -1,20 +1,21 @@
 class Solution {
     public String mergeAlternately(String word1, String word2) {
-        int len1 = word1.length();
-        int len2 = word2.length();
-        StringBuilder output = new StringBuilder(len1 + len2);
+        int word1Length = word1.length();
+        int word2Length = word2.length();
+        StringBuilder output = new StringBuilder(word1Length + word2Length);
 
         int i = 0;
-        while (i < len1 && i < len2) {
+        while (word1Length > i && word2Length > i) {
             output.append(word1.charAt(i));
             output.append(word2.charAt(i));
             i++;
         }
 
-        if (i < len1) {
-            output.append(word1, i, len1);
-        } else if (i < len2) {
-            output.append(word2, i, len2);
+        if (word1Length > i) {
+            output.append(word1, i, word1Length);
+        } else if (word2Length > 2) {
+            output.append(word2, i, word2Length);
+
         }
 
         return output.toString();
