e28621b133c440838bc7001f44677a28ac0afc26
Ahmed Hussein <ahmeddhus@gmail.com>
2025-06-17 08:39:06 +0300

    [1071] [Time Beats: 100.00%] [Memory Beats: 79.15%] - LeetPush

---
+++ Greatest-Common-Divisor-of-Strings.java
@@ -0,0 +1,29 @@
+public class Solution {
+    public String gcdOfStrings(String str1, String str2) {
+        if (!areConcatEqual(str1, str2)) {
+            return "";
+        }
+
+        int gcdLength = gcd(str1.length(), str2.length());
+        return str1.substring(0, gcdLength);
+    }
+
+    private boolean areConcatEqual(String a, String b) {
+        StringBuilder sb1 = new StringBuilder(a.length() + b.length());
+        sb1.append(a).append(b);
+
+        StringBuilder sb2 = new StringBuilder(a.length() + b.length());
+        sb2.append(b).append(a);
+
+        return sb1.toString().equals(sb2.toString());
+    }
+
+    private int gcd(int a, int b) {
+        while (b != 0) {
+            int tmp = a % b;
+            a = b;
+            b = tmp;
+        }
+        return a;
+    }
+}
