aa26dfa66cd3ff208c0d3c0d4e2c80ac7e984b81
Ahmed Hussein <ahmeddhus@gmail.com>
2025-06-24 10:59:14 +0300

    [238] [Time Beats: 88.81%] [Memory Beats: 97.62%] - LeetPush

--- Product-of-Array-Except-Self.java
+++ Product-of-Array-Except-Self.java
@@ -1,19 +1,18 @@
 class Solution {
     public int[] productExceptSelf(int[] nums) {
-        int n = nums.length;
-        int[] output = new int[n];
+        int[] answer = new int[nums.length];
+        answer[0] = 1;
 
-        output[0] = 1;
-        for (int i = 1; i < n; i++) {
-            output[i] = nums[i - 1] * output[i - 1];
+        for (int i = 1; i < nums.length; i++) {
+            answer[i] = answer[i - 1] * nums[i - 1];
         }
 
         int r = 1;
-        for (int i = n - 2; i >= 0; i--) {
-            r  *= nums[i+1];
-            output[i] *= r;
+        for (int i = nums.length - 2; i >= 0; i--) {
+            r *= nums[i + 1];
+            answer[i] *= r;
         }
 
-        return output;
+        return answer;
     }
 }
