53776967004f956ac73ed1446b401dd7f89ee3ba
Ahmed Hussein <ahmeddhus@gmail.com>
2025-06-25 10:41:21 +0300

    [334] [Time Beats: 99.90%] [Memory Beats: 30.69%] - LeetPush

--- Increasing-Triplet-Subsequence.java
+++ Increasing-Triplet-Subsequence.java
@@ -1,12 +1,22 @@
 class Solution {
     public boolean increasingTriplet(int[] nums) {
-       int min1 = Integer.MAX_VALUE;
-       int min2 = Integer.MAX_VALUE;
-       for(int n : nums) {
-           if(n <= min1) min1 = n;
-           else if(n <= min2) min2 = n;
-           else return true;
-       }
-       return false;
+        if(nums.length < 3) {
+            return false;
+        }
+
+        int first = Integer.MAX_VALUE;
+        int second = Integer.MAX_VALUE;
+
+        for (int n : nums) {
+            if (n <= first) {
+                first = n;
+            }else if (n <= second) {
+                second = n;
+            }else {
+                return true;
+            }
+        }
+
+        return false;
     }
 }
